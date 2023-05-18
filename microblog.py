ad066cad62abf546de7e7f965902e6c1b698d49e
Stephen Shanko <scubashanko@gmail.com>
2023-05-17 23:24:46 -0400

    Update microblog.py

--- microblog.py
+++ microblog.py
@@ -1,3 +1,7 @@
 from api import create_app
 
+## Added Seeker agent to the Python app and started the agent before starting the Flask Application
+import seeker
+seeker.start()
+
 app = create_app()
